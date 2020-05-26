765d75adca8d144b44816aef83b6bffa6876e7e0
mpadge <mark.padgham@email.com>
2020-05-26 16:05:12 +0200

    add files arg to add_contributions

--- R/add-contributors.R
+++ R/add-contributors.R
@@ -3,11 +3,14 @@
 #' Add contributors to README.Rmd
 #'
 #' @param ncols Number of columns for contributors in 'README'
+#' @param files Names of files in which to add contributors
 #' @param alphabetical If `TRUE`, order contributors alphabetically, otherwise
 #' order by decreasing numbers of contributions.
 #' @return Nothing.
 #' @export
-add_contributors <- function (ncols = 7, alphabetical = FALSE) {
+add_contributors <- function (ncols = 7,
+                              files = c ("README.Rmd", "README.md"),
+                              alphabetical = FALSE) {
     if (!git2r::in_repository ())
         stop ("This does not appear to be a git repository")
 
@@ -21,8 +24,11 @@ add_contributors <- function (ncols = 7, alphabetical = FALSE) {
     x <- get_contributors (or$org, or$repo, alphabetical = alphabetical)
 
     x <- x [which (!is.na (x$login)), ]
-    contribs_to_readme (x, orgrepo = or, ncols = ncols, rmd = TRUE)
-    contribs_to_readme (x, orgrepo = or, ncols = ncols, rmd = FALSE)
+    files <- file.path (here::here(), files)
+    files <- files [which (file.exists (files))]
+
+    chk <- lapply (files, function (i)
+            contribs_to_readme (x, orgrepo = or, ncols = ncols, filename = i))
 }
 
 get_org_repo <- function (remote) {
@@ -32,35 +38,39 @@ get_org_repo <- function (remote) {
           repo = repo)
 }
 
-contribs_to_readme <- function (dat, orgrepo, ncols, rmd = TRUE) {
-    if (rmd)
-        f0 <- "README.Rmd"
-    else
-        f0 <- "README.md"
-
-    f <- file.path (here::here (), f0)
-    if (!file.exists (f))
-        return (NULL)
-        #stop ("Unable to find ", f0)
-
-    x <- readLines (f)
+contribs_to_readme <- function (dat, orgrepo, ncols, filename) {
+    x <- readLines (filename)
 
     contribs_sec <- grep ("# Contributors$", x)
-    if (length (contribs_sec) != 1)
-        stop ("README must have a section titled 'Contributors'")
+    has_contribs_sec <- length (contribs_sec) == 1
+    if (!has_contribs_sec) {
+        fshort <- utils::tail (strsplit (filename, "/") [[1]], 1)
+        message ("File [", fshort, "] has no section titled 'Contributors'; ",
+                 "Table will be added to bottom of file.")
+        contribs_sec <- length (x)
+    }
 
     contribs_start <- grep ("<!-- ALL-CONTRIBUTORS-LIST:START", x)
     contribs_end <- grep ("<!-- ALL-CONTRIBUTORS-LIST:END", x)
     if (length (contribs_start) == 1 & length (contribs_end) == 1) {
         xtop <- x [1:(contribs_start - 1)]
-        xbottom <- x [(contribs_end + 1):length (x)]
-        
+        xbottom <- NULL
+        if (contribs_end < length (x))
+            xbottom <- x [(contribs_end + 1):length (x)]
+
     } else {
         xtop <- x [1:contribs_sec]
-        xbottom <- x [(contribs_sec + 1):length (x)]
+        xbottom <- NULL
+        if (contribs_sec < length (x))
+            xbottom <- x [(contribs_sec + 1):length (x)]
     }
 
-    xmid <- c ("<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->",
+    xmid <- NULL
+    if (!has_contribs_sec)
+        xmid <- c ("## Contributors", "")
+
+    xmid <- c (xmid,
+               "<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->",
                "<!-- prettier-ignore-start -->",
                "<!-- markdownlint-disable -->",
                "",
@@ -108,13 +118,14 @@ contribs_to_readme <- function (dat, orgrepo, ncols, rmd = TRUE) {
                "",
                "<!-- markdownlint-enable -->",
                "<!-- prettier-ignore-end -->",
-               "<!-- ALL-CONTRIBUTORS-LIST:END -->")
+               "<!-- ALL-CONTRIBUTORS-LIST:END -->",
+               "")
 
     txt <- c (xtop, xmid, xbottom)
-    con <- file (f, "w")
+    con <- file (filename, "w")
     writeLines (txt, con = con)
     close (con)
 
-    message ("contributors written to [", f, "]")
+    message ("contributors written to [", filename, "]")
 }
 
